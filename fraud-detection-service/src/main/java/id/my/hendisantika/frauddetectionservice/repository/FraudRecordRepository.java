4f9775f600de84f63905527f65bc21a8d22f07d2
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 08:28:37 +0700

    Add existsByCustomerId in FraudRecordRepository🫸🌀✏️📗🐧🐳⬆

--- fraud-detection-service/src/main/java/id/my/hendisantika/frauddetectionservice/repository/FraudRecordRepository.java
+++ fraud-detection-service/src/main/java/id/my/hendisantika/frauddetectionservice/repository/FraudRecordRepository.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.frauddetectionservice.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +20,17 @@ import org.springframework.stereotype.Repository;
 public class FraudRecordRepository {
 
     private final JdbcClient jdbcClient;
+
+    @Transactional(readOnly = true)
+    public boolean existsByCustomerId(int customerId) {
+        var sql = """
+                SELECT COUNT(*) AS fraud__record_exists
+                FROM fraud_records
+                WHERE customerid = :customerid;
+                """;
+        return jdbcClient.sql(sql)
+                .param("customerId", customerId)
+                .query(Integer.class)
+                .single() > 0;
+    }
 }
