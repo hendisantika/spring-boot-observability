895f1191bb71a33aaf87d8e78a89743e4f35b8cf
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 08:36:49 +0700

    Add LoanStatus Enum🫸🌀✏️📗🐧🐳⬆

---
+++ loan-service/src/main/java/id/my/hendisantika/loanservice/entity/LoanStatus.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.loanservice.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-observability
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/01/25
+ * Time: 08.36
+ * To change this template use File | Settings | File Templates.
+ */
+public enum LoanStatus {
+    APPROVED, REJECTED
+}
