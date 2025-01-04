3ebc8e12d6acfdcd12740926ddd53af5b67b17e9
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 08:18:21 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/observability/SpringBootObservabilityApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootObservabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootObservabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/observability/SpringBootObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
