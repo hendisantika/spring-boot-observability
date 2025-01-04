ab28488f98c4383b15d3fb5e9244b9c86a85191f
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 08:40:28 +0700

    Add LoanService🫸🌀✏️📗🐧🐳⬆

---
+++ loan-service/src/main/java/id/my/hendisantika/loanservice/service/LoanService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.loanservice.service;
+
+import id.my.hendisantika.loanservice.client.FraudDetectionClient;
+import id.my.hendisantika.loanservice.repository.LoanRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-observability
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/01/25
+ * Time: 08.39
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class LoanService {
+    private final FraudDetectionClient fraudDetectionClient;
+    private final LoanRepository loanRepository;
+}
