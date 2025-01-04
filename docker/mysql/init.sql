bf47af8c9354dd6407147b8a5b578f9f5c62a62c
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 23:10:57 +0700

    fix: Docker compose.yml🫸🌀✏️📗🐧🐳⬆

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,2 +1,15 @@
 CREATE SCHEMA loan_service;
 CREATE SCHEMA fraud_detection;
+GRANT
+ALL
+PRIVILEGES
+ON
+loan_service
+.
+*
+TO
+'yu71'@'%';
+GRANT ALL PRIVILEGES ON fraud_detection.* TO
+'yu71'@'%';
+FLUSH
+PRIVILEGES;
