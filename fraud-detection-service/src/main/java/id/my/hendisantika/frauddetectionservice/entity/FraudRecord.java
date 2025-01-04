a13c57593ae4555d6437fcfee5946bc6223660dc
hendisantika <hendisantika@yahoo.co.id>
2025-01-04 08:27:31 +0700

    Add FraudRecord Entity🫸🌀✏️📗🐧🐳⬆

---
+++ fraud-detection-service/src/main/java/id/my/hendisantika/frauddetectionservice/entity/FraudRecord.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.frauddetectionservice.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-observability
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/01/25
+ * Time: 08.27
+ * To change this template use File | Settings | File Templates.
+ */
+public record FraudRecord(Long id, String fraudRecordId, int customerId, LoanStatus loanStatus) {
+}
